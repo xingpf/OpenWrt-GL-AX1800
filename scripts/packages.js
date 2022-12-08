35c369868276f0e26088a98391914200cb3eae3c
xingpf <xingpf@mochasoft.com.cn>
2022-12-08 10:08:00 +0800

    1

--- scripts/packages.js
+++ scripts/packages.js
@@ -5,62 +5,62 @@
  * @desc app 描述
  */
 module.exports = [
-  {
-    name: 'luci-theme-argon',
-    desc: 'argon主题',
-  },
-  {
-    name: 'luci-app-argon-config',
-    desc: 'argon主题设置',
-  },
+  // {
+  //   name: 'luci-theme-argon',
+  //   desc: 'argon主题',
+  // },
+  // {
+  //   name: 'luci-app-argon-config',
+  //   desc: 'argon主题设置',
+  // },
 //   {
 //     name: 'luci-app-store',
 //     desc: '应用商店',
 //   },
-  {
-    name: 'luci-app-ramfree',
-    desc: '释放内存',
-  },
-  {
-    name: 'luci-app-wol',
-    desc: 'wol 网络唤醒',
-  },
-  {
-    name: 'luci-app-diskman',
-    desc: '磁盘管理工具',
-  },
-  {
-    name: 'luci-app-smartdns',
-    desc: 'smartdns DNS防污染',
-  },
-  {
-    name: 'luci-app-openclash',
-    desc: 'clash的图形代理软件',
-  },
-  {
-    name: 'luci-app-aria2',
-    desc: 'aria2下载工具',
-  },
-  {
-    name: 'luci-app-zerotier',
-    desc: 'ZeroTier内网穿透',
-  },
-  {
-    name: 'luci-app-turboacc',
-    desc: 'Turbo ACC 网络加速(支持 Fast Path 或者 硬件 NAT)',
-  },
-  {
-    name: 'luci-app-xlnetacc',
-    desc: '迅雷快鸟',
-  },
+//   {
+//     name: 'luci-app-ramfree',
+//     desc: '释放内存',
+//   },
+//   {
+//     name: 'luci-app-wol',
+//     desc: 'wol 网络唤醒',
+//   },
+//   {
+//     name: 'luci-app-diskman',
+//     desc: '磁盘管理工具',
+//   },
+//   {
+//     name: 'luci-app-smartdns',
+//     desc: 'smartdns DNS防污染',
+//   },
+//   {
+//     name: 'luci-app-openclash',
+//     desc: 'clash的图形代理软件',
+//   },
+//   {
+//     name: 'luci-app-aria2',
+//     desc: 'aria2下载工具',
+//   },
+//   {
+//     name: 'luci-app-zerotier',
+//     desc: 'ZeroTier内网穿透',
+//   },
+//   {
+//     name: 'luci-app-turboacc',
+//     desc: 'Turbo ACC 网络加速(支持 Fast Path 或者 硬件 NAT)',
+//   },
+//   {
+//     name: 'luci-app-xlnetacc',
+//     desc: '迅雷快鸟',
+//   },
 //   {
 //     name: 'luci-app-appfilter',
 //     desc: '应用过滤'
 //   },
-  {
-    name: 'luci-app-cpufreq',
-    desc: 'CPU 性能优化调节',
-  },
+//   {
+//     name: 'luci-app-cpufreq',
+//     desc: 'CPU 性能优化调节',
+//   },
 //   {
 //     name: 'luci-app-samba4',
 //     desc: 'samba4文件共享',
@@ -73,10 +73,10 @@ module.exports = [
 //     name: 'luci-app-advancedsetting',
 //     desc: '系统高级设置',
 //   },
-  {
-    name: 'luci-app-ttyd',
-    desc: 'ttyd 终端',
-  },
+//   {
+//     name: 'luci-app-ttyd',
+//     desc: 'ttyd 终端',
+//   },
 //   {
 //     name: 'luci-app-unblockneteasemusic',
 //     desc: '解锁网易云音乐 (无界面)',
@@ -86,12 +86,12 @@ module.exports = [
 //     desc: '多拨虚拟网卡（需要光猫支持多拨）',
 //   },
   {
-    name: 'luci-app-passwall',
-    desc: 'passwall代理软件',
+    name: 'luci-app-vssr',
+    desc: 'vssr代理软件',
   },
   {
     name: 'luci-app-ssr-plus',
-    desc: 'ssr-plus 代理软件',
+    desc: 'ssr-plus代理软件',
   },
 //   {
 //     name: 'luci-app-aliddns',
@@ -125,14 +125,14 @@ module.exports = [
 //     name: 'luci-app-wrtbwmon',
 //     desc: '实时流量监测',
 //   },
-  {
-    name: 'luci-app-serverchan',
-    desc: '微信/Telegram推送',
-  },
-  {
-    name: 'luci-app-transmission',
-    desc: 'BT下载工具',
-  },
+//   {
+//     name: 'luci-app-serverchan',
+//     desc: '微信/Telegram推送',
+//   },
+//   {
+//     name: 'luci-app-transmission',
+//     desc: 'BT下载工具',
+//   },
 //   {
 //     name: 'luci-app-jd-dailybonus',
 //     desc: '京东签到服务',
